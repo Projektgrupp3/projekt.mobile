809bd43e0bd1928448d3dec63971a1ec2c61e520
Emil Bergwik <bergwik@gmail.com>
2011-11-08 14:28:21 +0100

    Testar tisdag eftermiddag

---
+++ src/tddd36/grupp3/controllers/MapController.java
@@ -0,0 +1,20 @@
+package tddd36.grupp3.controllers;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MapController implements Runnable, Observer{
+
+	
+	
+	public void update(Observable observable, Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/tddd36/grupp3/models/ClientModel.java
@@ -0,0 +1,7 @@
+package tddd36.grupp3.models;
+
+import java.util.Observable;
+
+public class ClientModel extends Observable{
+	
+}
