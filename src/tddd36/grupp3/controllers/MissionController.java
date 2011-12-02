8fb15601cdbf3d36ec72075e4f365d9c524e1b38
ThomasSchytzler <thosc494@student.liu.se>
2011-12-02 16:49:43 +0100

    xmlvy och verifieringsrapport edittext

--- src/tddd36/grupp3/controllers/MissionController.java
+++ src/tddd36/grupp3/controllers/MissionController.java
@@ -7,29 +7,37 @@ import tddd36.grupp3.models.MissionModel;
 import tddd36.grupp3.resources.Event;
 import tddd36.grupp3.views.MainView;
 import tddd36.grupp3.views.MissionTabView;
+import tddd36.grupp3.views.UpdateMission;
 
 import com.google.android.maps.GeoPoint;
 
 public class MissionController implements Observer {
 	MissionTabView mv;
 	MissionModel mm;
-	
+	MissionModel mm1;
+
+	private UpdateMission updateMission;
+
 	public MissionController(MissionTabView mv){
 		this.mv = mv;
 		mm = new MissionModel(mv, this);
 	}
+
 	public void update(Observable observable, Object data) {
 		// TODO Auto-generated method stub
-		
+
 	}
 	public GeoPoint getCurrentMissionAddress(){
 		return mm.getCurrentGeoPoint();
 	}
 	public void setCurrentMission(Event ev){
 		if(ev != null){
-		mm.setCurrentMission(ev);
-		MainView.db.addRow(ev);
+			mm.setCurrentMission(ev);
+			MainView.db.addRow(ev);
 		}
+
+
 	}
+
 
 }
