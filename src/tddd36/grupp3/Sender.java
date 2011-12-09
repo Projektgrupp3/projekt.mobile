0969250647feca346e55867a0cfda6e32c5b44f4
David Buö <davidbauwie@gmail.com>
2011-12-09 09:57:07 +0100

    HEJHEJEHEJHEJEHJEHEJ

--- src/tddd36/grupp3/Sender.java
+++ src/tddd36/grupp3/Sender.java
@@ -41,7 +41,7 @@ public class Sender {
 	public static final String ACK_CHOSEN_UNIT = "ACK_CHOSEN_UNIT";
 	public static final String LOG_OUT = "LOG_OUT";
 
-	private static final String COM_IP = "130.236.226.78";
+	private static final String COM_IP = "130.236.226.46";
 	private static final int COM_PORT = 1560;
 	
 	public static String NETWORK_STATUS;
